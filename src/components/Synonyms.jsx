7cc3763b0181ffee5891f4335fe761fc9429f5ad
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-05-10 14:56:33 +0200

    Update Synonyms.jsx

--- src/components/Synonyms.jsx
+++ src/components/Synonyms.jsx
@@ -1,7 +1,15 @@
 export default function Synonyms(props) {
   console.log(props.synonyms);
   if (props.synonyms) {
-    return <div>{props.synonyms}</div>;
+    return (
+      <div>
+        <ul className="synonyms">
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
   } else {
     return null;
   }
